f9476d5719128efefa966ad9820a81b3f0e57467
Gerard <gerard.juijn@epay.ingenico.com>
2019-02-19 17:03:00 +0100

    Initial commit

---
+++ config.go
@@ -0,0 +1,16 @@
+package igconfig
+
+import (
+	_ "strings"
+	_ "fmt"
+	_ "errors"
+	"finops/utils/iglog"
+)
+
+func LoadConfig(l iglog.Log, c *interface{}) error {
+	l.DebugFull("LoadConfig start")
+
+
+
+	l.DebugFull("LoadConfig end")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module igdcs.com/finops/utils/basics/igconfig
+
+require gitlab.test.igdcs.com/finops/utils/basics/igstrings v0.0.1
