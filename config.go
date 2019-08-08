a612c73e2a88d2fb557d663709513e80e0b3279d
Gerard Juijn <gerard.juijn@epay.ingenico.com>
2019-08-08 14:28:14 +0200

    Add individual functions for loading config defaults, from a file, from environment and from command-line

--- config.go
+++ config.go
@@ -6,11 +6,6 @@ import (
 	"strings"
 )
 
-// Printer interface can be supplied by a calling process to print the configuration after it has been loaded
-type Printer interface {
-	PrintConfig(c interface{})
-}
-
 // myConfig contains local variables during config processing
 type myConfig struct {
 	c        interface{}
@@ -19,8 +14,64 @@ type myConfig struct {
 	f        reflect.StructField
 }
 
-// LoadConfig loads configuration parameters from a file, the environment and finally from
+// LoadConfigDefaults loads a configuration struct with its defaults.
+func LoadConfigDefaults(c interface{}) error {
+	data := myConfig{c: c, warnings: make([]string, 0)}
+	if err := data.loadDefaults(); err != nil {
+		return err
+	}
+
+	if len(data.warnings) > 0 {
+		return fmt.Errorf("LoadConfigDefaults warnings found:\n%s", strings.Join(data.warnings, "\n"))
+	}
+
+	return nil
+}
+
+// LoadConfigFile loads a configuration struct from a file.
+func LoadConfigFile(c interface{}, file string) error {
+	data := myConfig{c: c, file: file, warnings: make([]string, 0)}
+
+	if err := data.loadFile(); err != nil {
+		return err
+	}
+
+	if len(data.warnings) > 0 {
+		return fmt.Errorf("LoadConfigFile warnings found:\n%s", strings.Join(data.warnings, "\n"))
+	}
+
+	return nil
+}
+
+// LoadConfigEnv loads a configuration struct from the environment.
+func LoadConfigEnv(c interface{}) error {
+	data := myConfig{c: c, warnings: make([]string, 0)}
+
+	data.loadEnv()
+
+	if len(data.warnings) > 0 {
+		return fmt.Errorf("LoadConfigEnv warnings found:\n%s", strings.Join(data.warnings, "\n"))
+	}
+
+	return nil
+}
+
+// LoadConfigCmdline loads a configuration struct from the command-line parameters.
+func LoadConfigCmdline(c interface{}) error {
+	data := myConfig{c: c, warnings: make([]string, 0)}
+
+	data.loadCmdline()
+
+	if len(data.warnings) > 0 {
+		return fmt.Errorf("LoadConfigCmdline warnings found:\n%s", strings.Join(data.warnings, "\n"))
+	}
+
+	return nil
+}
+
+// LoadConfig loads a configuration struct from a file, the environment and finally from
 // command-line parameters (the latter override the former) into a config struct.
+// This is a convenience function encapsulating all individual functions above.
 func LoadConfig(c interface{}, file string, env, cmd bool) error {
 	data := myConfig{c: c, file: file, warnings: make([]string, 0)}
 
@@ -30,7 +81,7 @@ func LoadConfig(c interface{}, file string, env, cmd bool) error {
 	}
 
 	if file != "" {
-		err = data.loadConfigFile()
+		err = data.loadFile()
 	}
 
 	if env {
@@ -41,24 +92,12 @@ func LoadConfig(c interface{}, file string, env, cmd bool) error {
 		data.loadCmdline()
 	}
 
-	if len(data.warnings) > 0 {
-		return fmt.Errorf("LoadConfig warnings found:\n%s", strings.Join(data.warnings, "\n"))
-	}
-
-	return err
-}
-
-// LoadConfigAndPrint loads configuration parameters from a file, the environment and finally from
-// command-line parameters (the latter override the former) into a config struct.
-// It will use the supplied Printer interface to allow the caller to print the configuration
-func LoadConfigAndPrint(c interface{}, file string, env, cmd bool, p Printer) error {
-	err := LoadConfig(c, file, env, cmd)
 	if err != nil {
 		return err
 	}
 
-	if p != nil {
-		p.PrintConfig(c)
+	if len(data.warnings) > 0 {
+		return fmt.Errorf("LoadConfig warnings found:\n%s", strings.Join(data.warnings, "\n"))
 	}
 
 	return nil
