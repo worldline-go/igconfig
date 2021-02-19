80eadd19a289a29c5e4c612cc8b51ebb522cda58
Yevhen Sylenko <yevhen.sylenko@epay.ingenico.com>
2021-02-19 13:14:21 +0100

    Add more tests for field traversing

--- internal/internal_test.go
+++ internal/internal_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type smallInnerStruct struct {
@@ -118,6 +119,81 @@ func TestStructIterator_Iterate(t *testing.T) {
 			}
 
 		})
+	}
+}
+
+func TestStructIterator_FieldNames(t *testing.T) {
+	tests := []struct {
+		name      string
+		input     interface{}
+		fieldFunc FieldNameFunc
+		result    []string
+	}{
+		{
+			name: "no tags",
+			input: &struct {
+				Inner struct {
+					FieldInner bool
+					unexported []string
+				}
+				Field1 string
+			}{},
+			fieldFunc: FieldNameWithSeparator("", "_", strings.ToUpper),
+			result:    []string{"INNER", "INNER_FIELDINNER", "FIELD1"},
+		},
+		{
+			name: "some tags",
+			input: &struct {
+				Inner struct {
+					FieldInner bool `env:"i"`
+					unexported []string
+				} `cfg:"rename"`
+				Field1 string `cmd:"flag"`
+			}{},
+			fieldFunc: FieldNameWithSeparator("env", "_", strings.ToUpper),
+			result:    []string{"RENAME", "RENAME_I", "FIELD1"},
+		},
+		{
+			name: "skip struct",
+			input: &struct {
+				Inner struct {
+					FieldInner bool `env:"i"`
+					unexported []string
+				} `cfg:"-"`
+				Field1 string `cmd:"flag"`
+			}{},
+			fieldFunc: FieldNameWithSeparator("env", "_", strings.ToUpper),
+			result:    []string{"-", "FIELD1"},
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+
+		t.Run(test.name, func(t *testing.T) {
+			fields := make([]string, 0, len(test.result))
+
+			iter := StructIterator{
+				FieldNameFunc: fieldCapture(test.fieldFunc, &fields),
+				IteratorFunc:  func(fieldName string, field reflect.Value) error { return nil },
+				Value:         test.input,
+			}
+
+			require.NoError(t, iter.Iterate())
+
+			require.Equal(t, test.result, fields)
+		})
+	}
+
+}
+
+func fieldCapture(nameFunc FieldNameFunc, namesSlice *[]string) FieldNameFunc {
+	return func(outerName string, currentField reflect.StructField) string {
+		name := nameFunc(outerName, currentField)
+
+		*namesSlice = append(*namesSlice, name)
+
+		return name
 	}
 }
 
